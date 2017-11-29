33e21376a334ebade13a39a7f353c6e3085f216e
Patience Gifford <plgifford@Patiences-MacBook-Air.local>
2017-11-29 07:09:53 -0500

    AHHH

---
+++ js.js
@@ -0,0 +1,12 @@
+function multiplyBy()
+{
+	num1 = document.getElementById("1stNumber").value;
+	num2 = document.getElementById("2ndNumber").value;
+	document.getElementById("Answer").innerHTML = num1 * num2;
+}
+function divideBy()
+{
+	num1 = document.getElementById("1stNumber").value;
+	num2 = document.getElementById("2ndNumber").value;
+	document.getElementById("Answer").innerHTML = num1 / num2;
+}
